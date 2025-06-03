e6e376a6d356e1fa4b9e62eac234d223a0dc35c3
Ian Kan <iwk5@cornell.edu>
2025-06-03 16:41:18 -0700

    Cleans up files

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,17 +1,8 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
 import cornell from './cornell.svg'
+import headshot from './headshot.jpeg'
+
 
 export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    logo,
-    cornell
+    cornell,
+    headshot,
 }
