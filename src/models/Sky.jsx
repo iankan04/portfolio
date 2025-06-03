cfb8033dfe9b3674286e762ddd68bcd936200ee1
Ian Kan <iwk5@cornell.edu>
2025-06-03 16:31:07 -0700

    Minor changes

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -2,7 +2,7 @@ import { useGLTF } from '@react-three/drei';
 import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 
-import skyScene from '../assets/3d/sky.glb'
+import skyScene from '../assets/3d/Sky.glb'
 
 const Sky = ({ isRotating }) => {
     const sky = useGLTF(skyScene)
